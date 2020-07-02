5fad2756e497042ce4c26bfb2d59ec760d471c2d
Rafal Ochyra <rafal.ochyra@gmail.com>
2020-07-02 20:18:19 +0200

    Updates

---
+++ precache-manifest.5004e843bef860c9022d68d10ef8e4b6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d79d555485416528749ea939f263c85c",
+    "url": "/Movies-and-TV-Shows-Database-react/index.html"
+  },
+  {
+    "revision": "0db2ca81067df4558ce4",
+    "url": "/Movies-and-TV-Shows-Database-react/static/css/main.b4edf451.chunk.css"
+  },
+  {
+    "revision": "27f77a5ee7931a32e525",
+    "url": "/Movies-and-TV-Shows-Database-react/static/js/2.3303bf04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movies-and-TV-Shows-Database-react/static/js/2.3303bf04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0db2ca81067df4558ce4",
+    "url": "/Movies-and-TV-Shows-Database-react/static/js/main.e5937577.chunk.js"
+  },
+  {
+    "revision": "f1d2e7a28b3e5c14dfb0",
+    "url": "/Movies-and-TV-Shows-Database-react/static/js/runtime-main.f784054e.js"
+  }
+]);
